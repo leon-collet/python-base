44926d827442043d0c5e0eff1616c992bbdffd6e
leon <leon-collet>
2021-03-24 15:16:45 +0300

    ДЗ 7 3 задание готово

--- Chaykin_Alex_dz_7/task_7_3.py
+++ Chaykin_Alex_dz_7/task_7_3.py
@@ -13,7 +13,26 @@
 # (они играют роль пространств имён); предусмотреть возможные исключительные ситуации; это реальная задача,
 # которая решена, например, во фреймворке django.
 
-import os
+import os, shutil
+
+root_folder = os.path.abspath('my_project')
+folder = os.path.join(root_folder, 'templates')
+os.chdir('my_project')
+if not os.path.exists('templates'):
+    os.mkdir('templates')
+
+for root, dirs, files in os.walk('.'):
+    if root.__contains__('templates') and not root.startswith('./templates'):
+        new_dir = root.split('templates')[1].strip('/')
+        copy_folder = os.path.join(folder, new_dir) if new_dir != '' else folder
+        os.chdir(root)
+        for copy_file in files:
+            if not os.path.exists(os.path.join(copy_folder, copy_file)):
+                shutil.copy2(copy_file, copy_folder)
+        for copy_dir in dirs:
+            if not os.path.exists(os.path.join(copy_folder, copy_dir)):
+                os.mkdir(os.path.join(copy_folder, copy_dir))
+        os.chdir(root_folder)
 
 
 
