e021b1d2d91b3318926a5dcd4b20690660b61249
leon <leon-collet>
2021-03-25 11:09:51 +0300

    Переделал 7_3 так как оказалось, что всё что написанная мной функция есть в shutil и называется copytree

--- Chaykin_Alex_dz_7/task_7_3.py
+++ Chaykin_Alex_dz_7/task_7_3.py
@@ -15,24 +15,9 @@
 
 import os, shutil
 
-root_folder = os.path.abspath('my_project')
-folder = os.path.join(root_folder, 'templates')
+folder = 'templates'
 os.chdir('my_project')
-if not os.path.exists('templates'):
-    os.mkdir('templates')
 
 for root, dirs, files in os.walk('.'):
-    if root.__contains__('templates') and not root.startswith('./templates'):
-        new_dir = root.split('templates')[1].strip('/')
-        copy_folder = os.path.join(folder, new_dir) if new_dir != '' else folder
-        os.chdir(root)
-        try:
-            for copy_file in files:
-                if not os.path.exists(os.path.join(copy_folder, copy_file)):
-                    shutil.copy2(copy_file, copy_folder)
-            for copy_dir in dirs:
-                if not os.path.exists(os.path.join(copy_folder, copy_dir)):
-                    os.mkdir(os.path.join(copy_folder, copy_dir))
-        except ValueError as e:
-            print(f'Ошибка со значением пути или именем файла/директории: {e}')
-        os.chdir(root_folder)
+    if root.endswith('templates') and not root.startswith('./templates'):
+        shutil.copytree(root, folder, dirs_exist_ok=True)
